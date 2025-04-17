de511a78da87902a8839b32b4033f8dcf4782bf7
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-04-17 11:32:47 +0700

    menambahkan 3 variabel intergeral

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -30,3 +30,32 @@ void input()
         cin >> arr[i];
     }
 }
+
+void mergeSort(int low, int high)
+{
+    if (low < high)
+    { // step 1.a
+        return;
+    }
+
+    int mid = (low + high) / 2; // step 2
+    // step 3
+    mergeSort(low, mid);      // step3.a
+    mergeSort(mid + 1, high); // step 3.b
+    // step 4
+    int i = low;     // step 4.a
+    int j = mid + 1; // step 4.b
+    int k = low;     // step 4.c
+
+    while (i <= mid && j <= high)
+    { // step 4.d
+        if (arr[i] <= arr[j])
+        { // step 4.d.i
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        { // step 4.d.ii
+            B[k] = arr[j];
+            j++;
+        }
