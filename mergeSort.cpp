57d3bda59e1c3219f1a60902afaa0e84d3c53f50
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-04-17 11:33:45 +0700

    fungsi input

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -59,3 +59,23 @@ void mergeSort(int low, int high)
             B[k] = arr[j];
             j++;
         }
+        k++;
+    }
+
+    while (j <= high)
+    {
+        B[k] = arr[j];
+        j++;
+        k++;
+    }
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
