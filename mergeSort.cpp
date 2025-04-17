d0d4d9756be5dc5c62d9ec1881594ada8216ed2c
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-04-17 11:31:03 +0700

    add libary

---
+++ mergeSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int arr[20], B[]; // Ukuran array maksimum 20
+int n;            // Variabel untuk panjang array
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang elemen array: ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20\n";
+        }
+    }
+
+    cout << "\n=====================";
+    cout << "\nInputkan isi elemen array";
+    cout << "\n=====================\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Elemen ke-" << i << " : ";
+        cin >> arr[i];
+    }
+}
